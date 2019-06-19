dd63416cb606006fa9db1fb067007d8c276515b4
LKB <DfAC@users.noreply.github.com>
2019-06-19 16:19:34 +0100

    added main function block

--- RINEXb2EMS.py
+++ RINEXb2EMS.py
@@ -1,31 +1,41 @@
-file = "M1231340.19b"
-output = "test.ems"
+#!/usr/bin/env python3
+"""
+Module to convert RINEXB to EMS format
+LKB(c) 2019
+"""
+import sys
 
-f_output = open(output,'w')
+def convertRINEXB2EMS(RINEX_b_file,EMS_output):
 
-with open(file, 'r') as RINEX_b:
-  for line in RINEX_b:
-    #find end of header
-    if line.strip() == "END OF HEADER": #find end of header
-        break
-  while True:
-    #read blocks of 3
-    message=RINEX_b.readline().split()
-    if not message: break #EOF
-    try:
-        if message[10]=='SBA': #SBAS message
-            for idx in list(range(2)):
-              message += RINEX_b.readline().split()
-              #remove 00.1
-        else:
-          for idx in list(range(2)): RINEX_b.readline() #skip next 2 lines
+    if EMS_output:
+        f_output = open(EMS_output,'w')
+    else:
+        f_output =  sys.stdout
 
-        #write output in EMS format  SV_ID DATE_TIME MESSAGE_ID MESSAGE
-        #seconds format is xx.1, this converts it to EMS one - xx
-        seconds = message[6].split(".")[0]
-        f_output.write(" ".join(message[0:6]) + " " +
-                       seconds + " " + message[11] + " " +
-                       "".join(message[12:]) +"\n")
-    except:
-        print("Reading error, possible EOF or corrupted message")
-f_output.close()
+    with open(RINEX_b_file, 'r') as RINEX_b:
+      for line in RINEX_b:
+        #find end of header
+        if line.strip() == "END OF HEADER": #find end of header
+            break
+      while True:
+        #read blocks of 3
+        message=RINEX_b.readline().split()
+        if not message: break #EOF
+        try:
+            if message[10]=='SBA': #SBAS message
+                for idx in list(range(2)):
+                  message += RINEX_b.readline().split()
+            else:
+              for idx in list(range(2)): RINEX_b.readline() #skip next 2 lines
+
+            #write output in EMS format  SV_ID DATE_TIME MESSAGE_ID MESSAGE
+            #seconds format is xx.1, this converts it to EMS one - xx
+            second = message[6].split(".")[0]
+            head = " ".join(message[0:6]) + " " + second + " " + message[11] + " "
+            EGNOSmessage = "".join(message[12:])
+            f_output.write(head + EGNOSmessage +"\n")
+        except:
+            print("Reading error, possibly corrupted message")
+
+    if f_output is  not sys.stdout: f_output.close()
+    return 0
---
+++ runConverter.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+"""
+Run converter tool
+Following metodology set by ROKUBUN
+"""
+
+import argparse
+import sys
+
+from RINEXb2EMS import convertRINEXB2EMS
+
+
+if __name__ == "__main__":
+
+
+    # Parse command line
+    parser = argparse.ArgumentParser(description=__doc__)
+
+    parser.add_argument('input_log', metavar='<input RINEX b file>', type=str,
+                        help="RINEX b 2.11 file")
+    parser.add_argument('--output', '-o', metavar='<output EMS file>', type=str, default= "out.ems",
+                        help="""
+                        Output EMS file.
+                        Default = out.ems.
+                        To write to to the standard output use -o \"\"
+                        """)
+
+
+    ##running code
+    args = parser.parse_args()
+    RINEXfile = args.input_log
+
+    print("writing{} to {}".format(RINEXfile,args.output))
+    convertRINEXB2EMS(RINEXfile,args.output)
+
+
+
+    sys.exit(0)
